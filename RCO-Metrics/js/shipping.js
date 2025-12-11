a7eebe8f7fc0dbb012e5e2e36852b820a3a55ebd
hhowell116 <81898056+hhowell116@users.noreply.github.com>
2025-12-10 18:39:55 -0600

    Create shipping.js

---
+++ RCO-Metrics/js/shipping.js
@@ -0,0 +1,10 @@
+// This file should contain the JavaScript from your shipping.html
+// Just copy the entire <script> content from shipping.html (without script tags)
+(async function(){
+  const SHEET_ID = '1-S1OU7nw_085IXVbfiOY5qDPuRcPx2RqQJg8pnOqU9I';
+  const GID = '165470098';
+  const CSV_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/export?format=csv&gid=${GID}`;
+  
+  // ... rest of your shipping.js code ...
+  // Copy everything from your shipping.html <script> tag here
+})();
